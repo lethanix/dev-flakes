32b5004b3eeabd069597360834d01ca4f52dad01
lethanix <me@lethanix.com>
2026-04-23 00:14:03 -0400

    Add BootDev devshell; Formatting

--- flake.nix
+++ flake.nix
@@ -13,25 +13,40 @@
         pkgs = import nixpkgs { inherit system; };
       in
       {
+        formatter = pkgs.nixpkgs-fmt;
         devShells = {
-            default = pkgs.mkShell {
-              name = "default";
-              buildInputs = with pkgs; [
-                  git
-              ];
-            };
+          default = pkgs.mkShell {
+            name = "default";
+            buildInputs = with pkgs; [
+              git
+            ];
+          };
 
-            elixir = pkgs.mkShell {
-              name = "potion";
-              buildInputs = with pkgs; [
-                beam28Packages.elixir_1_19
-              ];
+          elixir = pkgs.mkShell {
+            name = "potion";
+            buildInputs = with pkgs; [
+              beam28Packages.elixir_1_19
+            ];
 
-              shellHook = ''
-                export ERL_AFLAGS="-kernel shell_history enabled"
-                echo "Elixir $(elixir --version | tail -1) ready"
-              '';
-            };
+            shellHook = ''
+              export ERL_AFLAGS="-kernel shell_history enabled"
+              echo "Elixir $(elixir --version | tail -1) ready"
+            '';
+          };
+
+          bootdev = pkgs.mkShell {
+            name = "boot.dev";
+            buildInputs = with pkgs; [
+              go
+            ];
+
+            shellHook = ''
+              go install github.com/bootdotdev/bootdev@latest
+              export PATH=$PATH:$HOME/go/bin
+              echo "Environment ready"
+              bootdev --version
+            '';
+          };
         };
       });
 }
